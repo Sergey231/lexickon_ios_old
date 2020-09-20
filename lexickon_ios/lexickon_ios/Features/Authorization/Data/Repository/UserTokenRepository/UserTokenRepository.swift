93bc542f55d32f64c58c78e18a53954af9decf02
Sergey Borovikov <sergey.borovikov@list.ru>
2020-09-20 12:57:18 +0500

    Refactoring for Authorizaitoin repositories

--- lexickon_ios/lexickon_ios/Features/Authorization/Data/Repository/UserTokenRepository/UserTokenRepository.swift
+++ lexickon_ios/lexickon_ios/Features/Authorization/Data/Repository/UserTokenRepository/UserTokenRepository.swift
@@ -1,12 +1,6 @@
-//
-//  UserTokenRepository.swift
-//  lexickon_ios
-//
-//  Created by Sergey Borovikov on 17.02.2020.
-//  Copyright © 2020 Sergey Borovikov. All rights reserved.
-//
+
+import LexickonApi
 
 final class UserTokenRepository {
     
 }
-
--- lexickon_ios/lexickon_ios/Features/Authorization/Data/Repository/UserTokenRepository/UserTokenRepositoryProtocol.swift
+++ lexickon_ios/lexickon_ios/Features/Authorization/Data/Repository/UserTokenRepository/UserTokenRepositoryProtocol.swift
@@ -1,10 +1,3 @@
-//
-//  UserTokenRepositoryProtocol.swift
-//  lexickon_ios
-//
-//  Created by Sergey Borovikov on 19.02.2020.
-//  Copyright © 2020 Sergey Borovikov. All rights reserved.
-//
 
 import LexickonApi
 import RxSwift
