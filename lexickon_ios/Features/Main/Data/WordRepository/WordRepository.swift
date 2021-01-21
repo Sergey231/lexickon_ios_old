a24112a84ba7dda6862089d7b84b53bec7229fbf
Sergey Borovikov <sergey.borovikov@list.ru>
2021-01-21 14:38:44 +0500

    Added params for adding word

--- lexickon_ios/Features/Main/Data/WordRepository/WordRepository.swift
+++ lexickon_ios/Features/Main/Data/WordRepository/WordRepository.swift
@@ -72,7 +72,7 @@ public final class WordRepository: WordRepositoryProtocol, ApiRepository {
         }
     }
     
-    public func add(_ word: LxWordCreate) -> Single<LxWordGet> {
+    public func add(_ word: LxWordCreate) -> Single<_LxWordGet> {
         
         guard let headers = headersWithAuthToken else {
             return .error(LxHTTPObject.Error.unauthorized)
@@ -80,24 +80,88 @@ public final class WordRepository: WordRepositoryProtocol, ApiRepository {
         
         let url = baseURL + "/api/words/"
         
+        let parametrs: Parameters = [
+            "studyWord" : word.studyWord,
+            "translates" : word.translates,
+            "image_key" : word.imageKey ?? "",
+            "image" : word.image
+        ]
+        
         return Single.create { single -> Disposable in
             
-            AF.request(url, method: .post, headers: headers)
-                .responseDecodable(
-                    of: LxWordGet.self,
-                    decoder: self.jsonDecoder
-                ) { res in
-                    
-                    switch res.result {
-                    case .success(let model):
-                        print(model)
-                        single(.success(model))
-                    case .failure(let failure):
-                        print(failure)
-                        single(.failure(LxHTTPObject.Error(with: res.response?.statusCode)))
-                    }
+            AF.request(url, method: .post, parameters: parametrs, headers: headers)
+                .response { res in
+                    let json = String.init(data: res.data!, encoding: .utf8)
+                    print(json ?? "JSON!")
                 }
+//                .responseDecodable(
+//                    of: _LxWordGet.self,
+//                    decoder: self.jsonDecoder
+//                ) { res in
+//
+//                    switch res.result {
+//                    case .success(let model):
+//                        print(model)
+//                        single(.success(model))
+//                    case .failure(let failure):
+//                        print(failure)
+//                        single(.failure(LxHTTPObject.Error(with: res.response?.statusCode)))
+//                    }
+//                }
             return Disposables.create()
+        }
+    }
+}
+
+
+public struct _LxWordGet: Codable {
+    
+    public init(
+        id: UUID,
+        studyWord: String,
+        translates: [String],
+        nextLessonDate: Date,
+        image: String
+    ) {
+        self.id = id
+        self.studyWord = studyWord
+        self.translates = translates
+        self.nextLessonDate = nextLessonDate
+        self.image = image
+    }
+    
+    public let id: UUID
+    public let studyWord: String
+    public let translates: [String]
+    public let nextLessonDate: Date
+    public let image: String
+    
+    private enum CodingKeys: String, CodingKey {
+        case id
+        case studyWord
+        case translates
+        case nextLessonDate
+        case image
+    }
+    
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+        id = try container.decode(UUID.self, forKey: .id)
+        studyWord = try container.decode(String.self, forKey: .studyWord)
+        translates = try container.decode([String].self, forKey: .translates)
+        image = try container.decode(String.self, forKey: .image)
+        
+        let nextLessonDateString = try container.decode(String.self, forKey: .nextLessonDate)
+        let dateFormatter = DateFormatter.iso8601
+        if let date = dateFormatter.date(from: nextLessonDateString) {
+            nextLessonDate = date
+        } else {
+            throw DecodingError.dataCorruptedError(
+                forKey: .nextLessonDate,
+                in: container,
+                debugDescription: "Date string does not match format expected by formatter."
+            )
         }
     }
 }
