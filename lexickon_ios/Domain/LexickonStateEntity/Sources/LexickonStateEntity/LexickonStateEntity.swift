255b47b4084742b44eeec5609fa4461cad97f144
Sergey Borovikov <sergey.borovikov@list.ru>
2021-05-21 19:40:05 +0500

    sync

--- lexickon_ios/Domain/LexickonStateEntity/Sources/LexickonStateEntity/LexickonStateEntity.swift
+++ lexickon_ios/Domain/LexickonStateEntity/Sources/LexickonStateEntity/LexickonStateEntity.swift
@@ -1,6 +1,24 @@
 
+//import LexickonApi
+
 public enum LexickonStateEntity {
     case hasReadyWords
     case hasFireWords
     case waiating
+    
+//    var fireWords: [] {
+//
+//    }
+//
+//    var readyWords: [] {
+//
+//    }
+//
+//    var newWords: [] {
+//
+//    }
+//
+//    var waitingWords: [] {
+//
+//    }
 }
