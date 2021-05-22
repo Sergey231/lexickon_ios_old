6fd4aec63b13773e79b87ecdbfd9655093fb7ae3
Sergey Borovikov <sergey.borovikov@list.ru>
2021-05-22 10:51:34 +0500

    LexickonStateEntity modified

--- lexickon_ios/Domain/LexickonStateEntity/Sources/LexickonStateEntity/LexickonStateEntity.swift
+++ lexickon_ios/Domain/LexickonStateEntity/Sources/LexickonStateEntity/LexickonStateEntity.swift
@@ -1,24 +1,54 @@
 
-//import LexickonApi
+import LexickonApi
 
-public enum LexickonStateEntity {
-    case hasReadyWords
-    case hasFireWords
-    case waiating
+public struct LexickonStateEntity {
     
-//    var fireWords: [] {
-//
-//    }
-//
-//    var readyWords: [] {
-//
-//    }
-//
-//    var newWords: [] {
-//
-//    }
-//
-//    var waitingWords: [] {
-//
-//    }
+    public enum State {
+        case hasReadyWords
+        case hasFireWords
+        case waiating
+        case empty
+    }
+    
+    private let words: [LxWordList]
+    
+    init(words: [LxWordList]) {
+        self.words = words
+    }
+    
+    public var state: State {
+        if !fireWords.isEmpty {
+            return .hasFireWords
+        } else if !readyWords.isEmpty {
+            return .hasReadyWords
+        } else if !words.isEmpty {
+            return .waiating
+        } else {
+            return .empty
+        }
+    }
+    
+    var fireWords: [WordEntity] {
+        words
+            .filter { $0.studyType == .fire }
+            .map { WordEntity(withLxWordList: $0) }
+    }
+
+    var readyWords: [WordEntity] {
+        words
+            .filter { $0.studyType == .ready }
+            .map { WordEntity(withLxWordList: $0) }
+    }
+
+    var newWords: [WordEntity] {
+        words
+            .filter { $0.studyType == .new }
+            .map { WordEntity(withLxWordList: $0) }
+    }
+
+    var waitingWords: [WordEntity] {
+        words
+            .filter { $0.studyType == .waiting }
+            .map { WordEntity(withLxWordList: $0) }
+    }
 }
