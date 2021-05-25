2b341e325fad31e0470c393fd78942c413fcaa8a
Sergey Borovikov <sergey.borovikov@list.ru>
2021-05-25 10:33:16 +0500

    LexickonStateInteractor refactoring

--- lexickon_ios/Domain/LexickonStateEntity/Sources/LexickonStateEntity/LexickonStateEntity.swift
+++ lexickon_ios/Domain/LexickonStateEntity/Sources/LexickonStateEntity/LexickonStateEntity.swift
@@ -10,45 +10,33 @@ public struct LexickonStateEntity {
         case empty
     }
     
-    private let words: [LxWordList]
+    let fireWordsCount: UInt
+    let readyWordsCount: UInt
+    let newWordsCount: UInt
+    let waitingWordsCount: UInt
     
-    init(words: [LxWordList]) {
-        self.words = words
+    init(
+        fireWordsCount: UInt,
+        readyWordsCount: UInt,
+        newWordsCount: UInt,
+        waitingWordsCount: UInt
+    ) {
+        self.fireWordsCount = fireWordsCount
+        self.readyWordsCount = readyWordsCount
+        self.newWordsCount = newWordsCount
+        self.waitingWordsCount = waitingWordsCount
     }
     
     public var state: State {
-        if !fireWords.isEmpty {
+        if fireWordsCount != 0 {
             return .hasFireWords
-        } else if !readyWords.isEmpty {
+        } else if readyWordsCount != 0 {
             return .hasReadyWords
-        } else if !words.isEmpty {
+        } else if waitingWordsCount != 0 {
             return .waiating
         } else {
             return .empty
         }
     }
     
-    var fireWords: [WordEntity] {
-        words
-            .filter { $0.studyType == .fire }
-            .map { WordEntity(withLxWordList: $0) }
-    }
-
-    var readyWords: [WordEntity] {
-        words
-            .filter { $0.studyType == .ready }
-            .map { WordEntity(withLxWordList: $0) }
-    }
-
-    var newWords: [WordEntity] {
-        words
-            .filter { $0.studyType == .new }
-            .map { WordEntity(withLxWordList: $0) }
-    }
-
-    var waitingWords: [WordEntity] {
-        words
-            .filter { $0.studyType == .waiting }
-            .map { WordEntity(withLxWordList: $0) }
-    }
 }
