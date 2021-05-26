a908674c741cc9b0d43e5a2d2e6b24d0b924bb95
Sergey Borovikov <sergey.borovikov@list.ru>
2021-05-26 10:56:38 +0500

    Implemented Words State Repository

--- lexickon_ios/Data/WordsStateRepository/Sources/WordsStateRepository/WordsStateRepository.swift
+++ lexickon_ios/Data/WordsStateRepository/Sources/WordsStateRepository/WordsStateRepository.swift
@@ -1,8 +1,33 @@
-
+import ApiRepository
+import LexickonApi
 import RxSwift
 
-public final class WordsStateRepository: WordsStateRepositoryProtocol {
+public final class WordsStateRepository: WordsStateRepositoryProtocol, ApiRepository {
+   
+    public init() {}
+    
     public func wordsState() -> Single<WordsState> {
-        .empty()
+        
+        let url = baseURL + "/api/wordsState"
+        let session = LxSessionManager.shared.session
+        
+        return Single.create { single -> Disposable in
+            
+            session.request(url)
+                .responseDecodable(
+                    of: WordsState.self,
+                    decoder: self.jsonDecoder
+                ) { res in
+                    
+                    switch res.result {
+                    case .success(let model):
+                        single(.success(model))
+                    case .failure:
+                        single(.failure(LxHTTPObject.Error(with: res.response?.statusCode)))
+                    }
+                }
+            
+            return Disposables.create()
+        }
     }
 }
