ddd4ca69040e3035dcd0eab64059b3827e6820c1
Sergey Borovikov <sergey.borovikov@list.ru>
2021-06-10 11:15:58 +0500

    Added base for WrodCardPresenter

---
+++ lexickon_ios/Presentation/WordCardFlow/WordCard/WordCardPresenter.swift
@@ -0,0 +1,26 @@
+//
+//  WordCardPresenter.swift
+//  lexickon_ios
+//
+//  Created by Sergey Borovikov on 10.06.2021.
+//  Copyright © 2021 Sergey Borovikov. All rights reserved.
+//
+
+import RxCocoa
+import Resolver
+
+final class WordCardPresenter {
+    
+    struct Input {
+        
+    }
+    
+    struct Output {
+        
+    }
+    
+    func configure(input: Input) -> Output {
+        
+        return Output()
+    }
+}
