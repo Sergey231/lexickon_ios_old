222cd2a3a9a7bfb2870bc125222e29677be21670
Sergey Borovikov <sergey.borovikov@list.ru>
2021-06-23 18:49:02 +0500

    Added scale for word card progress bar

--- lexickon_ios/Presentation/LXControlKit/Sources/LXControlKit/Views/WordCard/WordCardProgressBarView.swift
+++ lexickon_ios/Presentation/LXControlKit/Sources/LXControlKit/Views/WordCard/WordCardProgressBarView.swift
@@ -19,8 +19,9 @@ public final class WordCardProgressBarView: UIView {
         createUI()
     }
     
-    private enum UIConstrant {
+    private enum UIConstants {
         static let wordLevelViewSize: CGFloat = 48
+        static let wordLevelBgViewSize: CGFloat = 56
     }
     
     public struct Input {
@@ -47,6 +48,9 @@ public final class WordCardProgressBarView: UIView {
     
     private let disposeBag = DisposeBag()
     
+    fileprivate let levelBgView = UIView()
+    fileprivate let scaleBgView = UIView()
+    fileprivate let scaleView = UIView()
     fileprivate let levelView = UIView()
     
     override init(frame: CGRect) {
@@ -60,13 +64,44 @@ public final class WordCardProgressBarView: UIView {
        
     private func createUI() {
         
-        levelView.setup {
-            $0.layer.cornerRadius = UIConstrant.wordLevelViewSize/2
+        levelBgView.setup {
+            $0.layer.cornerRadius = UIConstants.wordLevelBgViewSize/2
             addSubview($0)
             $0.snp.makeConstraints {
-                $0.left.equalTo(4)
-                $0.top.equalTo(4)
-                $0.size.equalTo(UIConstrant.wordLevelViewSize)
+                $0.left.equalToSuperview()
+                $0.top.equalToSuperview()
+                $0.size.equalTo(UIConstants.wordLevelBgViewSize)
+            }
+        }
+        
+        scaleBgView.setup {
+            $0.layer.cornerRadius = 6
+            addSubview($0)
+            $0.snp.makeConstraints {
+                $0.centerY.equalTo(levelBgView.snp.centerY)
+                $0.height.equalTo(22)
+                $0.right.equalToSuperview()
+                $0.left.equalToSuperview().offset(Margin.regular)
+            }
+        }
+        
+        levelView.setup {
+            $0.layer.cornerRadius = UIConstants.wordLevelViewSize/2
+            addSubview($0)
+            $0.snp.makeConstraints {
+                $0.center.equalTo(levelBgView.snp.center)
+                $0.size.equalTo(UIConstants.wordLevelViewSize)
+            }
+        }
+        
+        scaleView.setup {
+            $0.layer.cornerRadius = 4
+            addSubview($0)
+            $0.snp.makeConstraints {
+                $0.centerY.equalTo(levelView.snp.centerY)
+                $0.height.equalTo(14)
+                $0.width.equalTo(4)
+                $0.left.equalTo(levelView.snp.right).offset(-4)
             }
         }
     }
@@ -75,6 +110,12 @@ public final class WordCardProgressBarView: UIView {
         
         input.studyState
             .drive(rx.studyState)
+            .disposed(by: disposeBag)
+        
+        parentViewController?.rx.viewDidAppear
+            .asSignal()
+            .map { _ in CGFloat(1) }
+            .emit(to: rx.progress)
             .disposed(by: disposeBag)
         
         return Output(didTapBack: .empty())
@@ -88,12 +129,36 @@ private extension Reactive where Base : WordCardProgressBarView {
             
             case .fire:
                 base.levelView.backgroundColor = Colors.fireWordBright.color
+                base.levelBgView.backgroundColor = Colors.fireWordPale.color
+                base.scaleBgView.backgroundColor = Colors.fireWordPale.color
+                base.scaleView.backgroundColor = Colors.fireWordBright.color
             case .ready:
                 base.levelView.backgroundColor = Colors.readyWordBright.color
+                base.levelBgView.backgroundColor = Colors.readyWordPale.color
+                base.scaleBgView.backgroundColor = Colors.readyWordPale.color
+                base.scaleView.backgroundColor = Colors.readyWordBright.color
             case .new:
                 base.levelView.backgroundColor = Colors.newWordBright.color
+                base.levelBgView.backgroundColor = Colors.newWord.color
+                base.scaleBgView.backgroundColor = Colors.newWord.color
+                base.scaleView.backgroundColor = Colors.newWordBright.color
             case .waiting:
                 base.levelView.backgroundColor = Colors.waitingWordBright.color
+                base.levelBgView.backgroundColor = Colors.waitingWordPale.color
+                base.scaleBgView.backgroundColor = Colors.waitingWordPale.color
+                base.scaleView.backgroundColor = Colors.waitingWordBright.color
+            }
+        }
+    }
+    
+    var progress: Binder<CGFloat> {
+        Binder(base) { base, progress in
+            // test implementation
+            UIView.animate(withDuration: 0.6) {
+                base.scaleView.snp.updateConstraints {
+                    $0.width.equalTo(4 + 260)
+                }
+                base.layoutIfNeeded()
             }
         }
     }
