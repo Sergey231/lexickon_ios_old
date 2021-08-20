c1a83203b8389e61b92f52be054c77464f769534
Sergey Borovikov <sergey.borovikov@list.ru>
2021-08-20 11:38:07 +0500

    Added files for Word View Exercise

---
+++ lexickon_ios/Presentation/MainFlow/ExercisesFlow/WordViewExercise/WordViewExerciseViewController.swift
@@ -0,0 +1,9 @@
+//
+//  WordViewExerciseViewController.swift
+//  lexickon_ios
+//
+//  Created by Sergey Borovikov on 20.08.2021.
+//  Copyright © 2021 Sergey Borovikov. All rights reserved.
+//
+
+import Foundation
