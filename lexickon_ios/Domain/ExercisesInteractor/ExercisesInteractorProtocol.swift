a5ea8a82ee346722063682fa483d3a6b266a55bb
Sergey Borovikov <sergey.borovikov@list.ru>
2021-08-30 11:16:12 +0500

    Added base for ExercisesSessionProtocol

---
+++ lexickon_ios/Domain/ExercisesInteractor/ExercisesInteractorProtocol.swift
@@ -0,0 +1,20 @@
+//
+//  ExercisesInteractorProtocol.swift
+//  lexickon_ios
+//
+//  Created by Sergey Borovikov on 30.08.2021.
+//  Copyright © 2021 Sergey Borovikov. All rights reserved.
+//
+
+import Foundation
+import LexickonStateEntity
+import RxSwift
+
+public struct ExercisesSessionEntity {
+    public let words: [WordEntity]
+//    public let exercises: [ExerciseEntity]
+}
+
+public protocol ExercisesInteractorProtocol {
+    func getExercisesSession() -> Single<ExercisesSessionEntity>
+}
