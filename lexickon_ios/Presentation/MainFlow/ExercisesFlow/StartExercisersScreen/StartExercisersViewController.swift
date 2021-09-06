a030b6644d080881ffe549123e1a6d7a1089304b
Sergey Borovikov <sergey.borovikov@list.ru>
2021-09-06 11:12:28 +0500

    Added base for start exercises screen

---
+++ lexickon_ios/Presentation/MainFlow/ExercisesFlow/StartExercisersScreen/StartExercisersViewController.swift
@@ -0,0 +1,9 @@
+//
+//  StartExercisersViewController.swift
+//  lexickon_ios
+//
+//  Created by Sergey Borovikov on 06.09.2021.
+//  Copyright © 2021 Sergey Borovikov. All rights reserved.
+//
+
+import Foundation
